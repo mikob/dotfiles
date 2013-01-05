71f23ff24814a576c143e120089a5dda8c7f0dbe
mborys <mikob@vt.edu>
2013-01-05 17:41:03 -0500

    Using virtual env. wrapper now

--- bin/idderall.sh
+++ bin/idderall.sh
@@ -4,6 +4,6 @@ cur_dir="$(pwd)"
 
 cd ~/workspace/shockcollar
 sublime &
-gnome-terminal --tab -e "bash -c \"cd ~/workspace/shockcollar-site/shockcollar;./manage.py runserver 8080;exec bash\""\
+gnome-terminal --tab -e "bash -c \"source /home/miko/.virtualenvs/shockcollar/bin/activate;cd ~/workspace/shockcollar-site/shockcollar;./manage.py runserver 8080;exec bash\""\
     --tab -e "bash -c \"cd ~/workspace/shockcollar;~/tools/addon-sdk-1.11/bin/cfx run -p ~/.mozilla/firefox/e0cvqqm9.Dev/ -b /opt/firefox-latest/firefox;echo '~/tools/addon-sdk-1.10/bin/cfx run -p ~/.mozilla/firefox/e0cvqqm9.Dev/ -b /opt/firefox-latest/firefox;';exec bash;\""\
     --tab -e "bash -c \"cd ~/workspace/shockcollar; exec bash\""

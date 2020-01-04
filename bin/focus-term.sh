2d6bc174b4384b9d511ea947cc30aa636ec6bb0f
mikob <fxrock2002@yahoo.com>
2020-01-03 23:02:43 -0500

    focus term working

--- bin/focus-term.sh
+++ bin/focus-term.sh
@@ -1,5 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 # use `xwininfo` to see window names
+TERM_WIN=`xdotool search --desktop 0 --name 'Terminal'`
+ACTIVE_WIN=`xdotool getactivewindow`
+if [ "$TERM_WIN" -eq "$ACTIVE_WIN" ]; then
+    xdotool windowminimize $TERM_WIN
+else
+    xdotool windowactivate $TERM_WIN
+fi
 
-TERM_WIN_ID=`xdotool search --name 'Terminal'`
-xdotool windowactivate $TERM_WIN_ID

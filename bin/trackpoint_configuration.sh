61a78146024e540b1ec3ac9a8173f2212c910f7a
mikob <fxrock2002@yahoo.com>
2020-07-07 21:21:20 -0500

    new trackpoint config

--- bin/trackpoint_configuration.sh
+++ bin/trackpoint_configuration.sh
@@ -1,3 +1,7 @@
-echo -n 254 > /sys/devices/platform/i8042/serio1/serio2/speed
-echo -n 254 > /sys/devices/platform/i8042/serio1/serio2/sensitivity
-echo -n 10 > /sys/devices/platform/i8042/serio1/serio2/inertia
+#echo -n 254 > /sys/devices/platform/i8042/serio1/serio2/speed
+#echo -n 254 > /sys/devices/platform/i8042/serio1/serio2/sensitivity
+#echo -n 10 > /sys/devices/platform/i8042/serio1/serio2/inertia
+
+echo -n 255 /sys/devices/platform/i8042/serio1/sensitivity
+xinput --set-prop 18 'libinput Accel Speed' 1
+

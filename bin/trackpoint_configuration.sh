932792e8d1bb5aefe7235980328c3b6ae6fb5682
mikob <fxrock2002@yahoo.com>
2020-08-09 10:43:34 -0700

    automatically finds trackpoint device

--- bin/trackpoint_configuration.sh
+++ bin/trackpoint_configuration.sh
@@ -3,5 +3,6 @@
 #echo -n 10 > /sys/devices/platform/i8042/serio1/serio2/inertia
 
 echo -n 255 /sys/devices/platform/i8042/serio1/sensitivity
-xinput --set-prop 18 'libinput Accel Speed' 1
+DEVICE_ID=`xinput | grep -i trackpoint | awk -F'\t' '{print $2}' | grep -Eo '[0-9]{1,}'`
+xinput --set-prop $DEVICE_ID 'libinput Accel Speed' 1
 

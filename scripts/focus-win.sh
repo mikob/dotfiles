213e1e0e44cf85968a13dbb7c6af487f51e5aae7
mikob <492864+mikob@users.noreply.github.com>
2023-12-24 13:38:25 -0600

    improvements

--- scripts/focus-win.sh
+++ scripts/focus-win.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 # use `xwininfo` to see window names
 TARGET_WORKSPACE=`xdotool get_desktop`
-TARGET_WIN=`xdotool search --desktop $TARGET_WORKSPACE --name "$1"`
+TARGET_WIN=`xdotool search --desktop $TARGET_WORKSPACE --class "$1"`
 ACTIVE_WIN=`xdotool getactivewindow`
-if [ "$TARGET_WIN" -eq "$ACTIVE_WIN" ]; then
+if [ -z "$TARGET_WIN" ]; then
+    # Window not found, start the program
+    nohup "$1" &>/dev/null &
+elif [ "$TARGET_WIN" -eq "$ACTIVE_WIN" ]; then
     xdotool windowminimize $TARGET_WIN
 else
     xdotool windowactivate $TARGET_WIN
